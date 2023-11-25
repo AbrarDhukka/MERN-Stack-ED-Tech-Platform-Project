46abe2d49b053f12fb8f6e002c9b7a49aa440d40
AbrarDhukka <abrdhk@gmail.com>
2023-11-25 15:10:07 +0530

    updated

--- studynotion-edtech-project-main/server/routes/Course.js
+++ studynotion-edtech-project-main/server/routes/Course.js
@@ -94,6 +94,7 @@ router.delete("/deleteCourse", deleteCourse)
 router.post("/createCategory", auth, isAdmin, createCategory)
 router.get("/showAllCategories", showAllCategories)
 router.post("/getCategoryPageDetails", categoryPageDetails)
+router.get("/getCategoryPageDetails", categoryPageDetails)
 
 // ********************************************************************************************************
 //                                      Rating and Review
